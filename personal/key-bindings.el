6c182b06a45063668cd971a4de9ada11363c923e
Raghav <Raghav_ftw@hotmail.com>
2024-08-31 00:18:02 +0530

    Require treemacs before checking if it's open or not

--- personal/key-bindings.el
+++ personal/key-bindings.el
@@ -3,15 +3,14 @@
 (defun my/treemacs-toggle ()
   "Toggle Treemacs. If Treemacs is open, select the window. If in Treemacs buffer, close it."
   (interactive)
+  (require 'treemacs)
   (if (treemacs-is-treemacs-window-selected?)
       (delete-window)
     (if (treemacs-get-local-window)
         (treemacs-select-window)
       (treemacs))))
 
-(with-eval-after-load 'treemacs
-  (global-set-key (kbd "C-1") 'my/treemacs-toggle))
-
+(global-set-key (kbd "C-1") 'my/treemacs-toggle)
 (global-set-key (kbd "C-3") 'magit-status)
 
 
